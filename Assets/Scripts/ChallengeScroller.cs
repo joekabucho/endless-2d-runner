89d8d37190595e4fbeba1e8363ca14c3c5ee40bd
joe kabucho <joekabucho2@gmail.com>
2019-01-14 10:18:37 +0300

    first game coomi

---
+++ Assets/Scripts/ChallengeScroller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeScroller : MonoBehaviour {
+
+	public float scrollSpeed =5.0f;
+	public GameObject[] Challenges;
+	public float frequency=0.5f;
+	float counter=0.0f;
+	public Transform challengesSpawnPoint;
+
+	bool isGameOver=false;
+	
+
+
+	// Use this for initialization
+	void Start () {
+		GenerateRandomChallenge();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//Generate gameobjects
+if (isGameOver)return;
+
+      if (counter<=0.0f){
+		  GenerateRandomChallenge();
+	  }
+else{
+	counter-=Time.deltaTime*frequency;
+}
+
+		//Scroll
+		GameObject currentChild;
+		for (int i=0;i<transform.childCount;i++){
+           currentChild=transform.GetChild(i).gameObject;
+		   ScrollChallenge(currentChild);
+		   if (currentChild.transform.position.x<=-15){
+			   Destroy(currentChild);
+		   }
+		}
+	}
+
+	void ScrollChallenge(GameObject currentChallenge){
+		currentChallenge.transform.position+=Vector3.left*(scrollSpeed *Time.deltaTime);
+	}
+
+	void GenerateRandomChallenge(){
+    GameObject newChallenge=  Instantiate(Challenges[Random.Range(0,Challenges.Length)],challengesSpawnPoint.position,Quaternion.identity) as GameObject;
+	newChallenge.transform.parent=transform;
+	counter =1.0f;
+	}
+
+	void GameOver(){
+		isGameOver=true;
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	public float jumpPower=10.0f;
+	Rigidbody2D myRigidBody;
+	private bool isGrounded=false;
+	float posX=0.0f;
+	bool isGameOver=false;
+	GameObject ChallengeController;
+	CharacterController myChallengeControler;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody=transform.GetComponent<Rigidbody2D>();
+		posX=transform.position.x;
+		myChallengeControler=GameObject.FindObjectOfType<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !isGameOver){
+            myRigidBody.AddForce(Vector3.up *(jumpPower *myRigidBody.mass *myRigidBody.gravityScale *20.0f));
+		}
+
+		if(transform.position.x<posX){
+			GameOver();
+		}
+	}
+
+	void Update(){
+
+	}
+	void GameOver(){
+        isGameOver=true;
+		
+	}
+	
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.collider.tag=="Ground")
+		{
+          isGrounded=true;
+		}
+	}
+	void OnCollisionStay2D(Collision2D other){
+		if (other.collider.tag=="Ground")
+		{
+          isGrounded=true;
+		}
+	}
+	
+	void OnCollisionExit2D(Collision2D other){
+		if (other.collider.tag=="Ground")
+		{
+          isGrounded=false;
+		}
+	}
+}
